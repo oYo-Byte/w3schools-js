a16122eea2c4101a974cbdecbfa9ec2a440c654a
oYo-Byte <282656323@qq.com>
2017-03-31 22:43:03 +0800

    03-31 commit

---
+++ 02.WhereTo/externalScript.js
@@ -0,0 +1,13 @@
+/*
+ * File:    externalScript.js
+ * 
+ * Created: 2017/03/31
+ * Author:  张志杰
+ * Email:   zhangzhijie@atlasdata.com.cn
+ * Copyright (C) 2017 海量云图（北京）数据技术有限公司.
+ */
+function externalFunc() {
+    document.getElementById("pEle").textContent = 'call the function in external file';
+}
+ 
+ 
